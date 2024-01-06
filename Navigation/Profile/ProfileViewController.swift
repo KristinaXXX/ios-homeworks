f9ccb93796433f7998b1a66904753523cbb2a397
KristinaXXX <107407324+KristinaXXX@users.noreply.github.com>
2024-01-06 16:42:03 +0500

    v2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -97,76 +97,93 @@ extension ProfileViewController: UITableViewDataSource {
 
 extension ProfileViewController: UITableViewDragDelegate {
     func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
-        let cellData = postData[indexPath.row]
-        let data = cellData.image.pngData()// .data(using: .utf8)
-        let itemProvider = NSItemProvider()
+        let post = postData[indexPath.row]
         
-        itemProvider.registerDataRepresentation(forTypeIdentifier: kUTTypePlainText as String, visibility: .all) { completion in
-            completion(data, nil)
-            return nil
-        }
-        
-        let stringProvider = NSString(string: cellData.description)
+        let imageProvider = NSItemProvider(object: post.image as UIImage)
+        let imageDragItem = UIDragItem(itemProvider: imageProvider)
+        imageDragItem.localObject = post.image
+
+        let descriptionProvider = NSItemProvider(object: post.description as NSString)
+        let descriptionDragItem = UIDragItem(itemProvider: descriptionProvider)
+        descriptionDragItem.localObject = post.description
 
         return [
-            UIDragItem(itemProvider: itemProvider),
-          //  UIDragItem(itemProvider: stringProvider)
+            imageDragItem,
+            descriptionDragItem
         ]
     }
 }
 
 extension ProfileViewController: UITableViewDropDelegate {
     func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        
         let destinationIndexPath: IndexPath
-            
         if let indexPath = coordinator.destinationIndexPath {
             destinationIndexPath = indexPath
         } else {
-            // Get last index path of table view.
+            // get from last row
             let section = tableView.numberOfSections - 1
             let row = tableView.numberOfRows(inSection: section)
             destinationIndexPath = IndexPath(row: row, section: section)
         }
-        
-        coordinator.session.loadObjects(ofClass: NSString.self) { items in
-            // Consume drag items.
-            let stringItems = items as! [String]
-            
-            var indexPaths = [IndexPath]()
-            for (index, item) in stringItems.enumerated() {
-                let indexPath = IndexPath(row: destinationIndexPath.row + index, section: destinationIndexPath.section)
-                //let newPost = Post(author: user.fullName, image: UIImage(data: Data( item) id: UUID())
-                //postData.append(<#T##newElement: Post##Post#>)
-                //self.model.addItem(item, at: indexPath.row)
-                indexPaths.append(indexPath)
+         
+        let rowInd = destinationIndexPath.row
+
+        let group = DispatchGroup()
+
+        var postDescription = String()
+        group.enter()
+        coordinator.session.loadObjects(ofClass: NSString.self) { objects in
+            let uStrings = objects as! [String]
+            for uString in uStrings {
+                postDescription = uString
+                break
             }
-
-
-            tableView.insertRows(at: indexPaths, with: .automatic)
+            group.leave()
         }
+
+        var postImage = UIImage()
+        group.enter()
+        coordinator.session.loadObjects(ofClass: UIImage.self) { objects in
+            let uImages = objects as! [UIImage]
+            for uImage in uImages {
+                postImage = uImage
+                break
+            }
+            group.leave()
+        }
+             
+        group.notify(queue: .main) {
+            // delete moved post if moved
+            if coordinator.proposal.operation == .move {
+                //postData.remove(at: self.postDragAtIndex)
+            }
+            // insert new post
+            let newPost = Post(author: self.user.fullName, description: postDescription, image: postImage, id: UUID())
+            postData.insert(newPost, at: rowInd)
+            
+            tableView.reloadData()
+        }
+        
+
     }
     
     func tableView(_ tableView: UITableView, canHandle session: UIDropSession) -> Bool {
-        session.canLoadObjects(ofClass: NSString.self)
+        session.canLoadObjects(ofClass: UIImage.self) && session.canLoadObjects(ofClass: NSString.self)
     }
     
     func tableView(_ tableView: UITableView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UITableViewDropProposal {
-        var dropProposal = UITableViewDropProposal(operation: .cancel)
-            
-        // Accept only one drag item.
-        guard session.items.count == 1 else { return dropProposal }
         
-        // The .move drag operation is available only for dragging within this app and while in edit mode.
-        if tableView.hasActiveDrag {
-            if tableView.isEditing {
-                dropProposal = UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
-            }
-        } else {
-            // Drag is coming from outside the app.
-            dropProposal = UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        guard session.items.count == 2 else {
+            return UITableViewDropProposal(operation: .cancel)
         }
 
-        return dropProposal
+        if tableView.hasActiveDrag {
+            return UITableViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        } else {
+            return UITableViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+        }
+
     }
 }
 
